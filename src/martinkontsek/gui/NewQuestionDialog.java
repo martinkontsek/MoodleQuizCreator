12b13e56292497cf3415cb444e677403cc0d4dea
martinkontsek <martinkontsek@gmail.com>
2015-07-22 12:35:34 +0200

    Cleanup.

--- src/martinkontsek/gui/NewQuestionDialog.java
+++ src/martinkontsek/gui/NewQuestionDialog.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package martinkontsek.gui;
 
 import javax.swing.DefaultComboBoxModel;
@@ -15,7 +10,7 @@ import martinkontsek.core.QuestionTypeEnum;
 
 /**
  *
- * @author Martin
+ * @author Martin Kontsek
  */
 public class NewQuestionDialog extends javax.swing.JDialog 
 {
